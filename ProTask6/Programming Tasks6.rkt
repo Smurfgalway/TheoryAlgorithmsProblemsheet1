3386683f91cfd4a8f73f858d64914aa0e1e28905
smurfgalway <g00323639@gmit.ie>
2018-04-08 15:39:03 +0100

    task6 added and working

--- ProTask6/Programming Tasks6.rkt
+++ ProTask6/Programming Tasks6.rkt
@@ -1,1 +1,20 @@
 #lang racket
+
+(define (hamming-distance ls ls2)
+  (if (null? ls)
+      0
+      (if
+       (= (car ls)(car ls2))
+          (hamming-distance (cdr ls)(cdr ls2))
+
+          (+ 1 (hamming-distance (cdr ls) (cdr ls2))
+       ))
+   )
+  )
+(display "Number of differences between lists as follows: \n")
+(hamming-distance (list 1 0 1 0 1 1 1 0) (list 1 1 1 1 0 0 0 0))
+(display "Number of differences between lists as follows: \n")
+(hamming-distance (list 1 1 1 1 1 1 1 1) (list 1 0 0 0 0 0 0 0))
+(display "Number of differences between lists as follows: \n")
+(hamming-distance (list 1 1 1 1 1 1 1 1) (list 1 1 1 1 1 1 1 1))
+

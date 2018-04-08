0bdf4c71b3f0e415da1348630c52fd439a6efeb8
smurfgalway <g00323639@gmit.ie>
2018-04-08 15:38:53 +0100

    task5 added and working

--- ProTask5/Programming Tasks5.rkt
+++ ProTask5/Programming Tasks5.rkt
@@ -1,1 +1,16 @@
 #lang racket
+
+(define (hamming-weight ls)
+  
+  (if (null? ls)
+       0
+       (+ (car ls) (hamming-weight (cdr ls))))
+      )
+
+(display "Hamming weight results as follows \n")
+
+(hamming-weight (list 1 0 1 0 1 1 1 0))
+(hamming-weight (list 0 0 0 0 0 0 0 0))
+(hamming-weight (list 1 1 1 1 1 1 1 1))
+(hamming-weight (list 1 1 1 0 1 0 1 0))
+

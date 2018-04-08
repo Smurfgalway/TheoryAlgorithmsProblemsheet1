695005bea4271243c8f9f75dc808d503db8b9564
smurfgalway <g00323639@gmit.ie>
2018-04-08 15:38:43 +0100

    task4 added and working

--- ProTask4/Programming Tasks4.rkt
+++ ProTask4/Programming Tasks4.rkt
@@ -1,1 +1,14 @@
 #lang racket
+
+(define (sumOf ls)
+  (if(not(null? ls))
+  (for/sum ((n ls)) n) 1))
+   
+  
+(define (sublsum ls)
+  (for ([j (in-list (combinations ls))])
+    (if(= 0 (sumOf j))
+       (display j)
+       '())))
+
+(sublsum (list 1 2 3 4 -5))

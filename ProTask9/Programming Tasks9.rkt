577f48b8b8a149a8c4b14f6036e39d8f88b822e6
smurfgalway <g00323639@gmit.ie>
2018-04-08 15:39:29 +0100

    task9 added and working

--- ProTask9/Programming Tasks9.rkt
+++ ProTask9/Programming Tasks9.rkt
@@ -1,1 +1,16 @@
 #lang racket
+
+(define (sod2 x y z)
+  (if (null? x)
+  '()
+  (cond
+    ((= 0 (modulo
+          (+ (car x)(car y)(car z)) 2))(cons 0
+                                  (sod2 (cdr x)(cdr y)(cdr z))))
+         (else (cons 1 (sod2 (cdr x)(cdr y) (cdr z)))
+          )
+         ))
+    )
+
+(display "Results are as follows: \n")
+(sod2 (list 0 0 0 0 1 1 1 1) (list 0 0 1 1 0 0 1 1) (list 0 1 0 1 0 1 0 1))
